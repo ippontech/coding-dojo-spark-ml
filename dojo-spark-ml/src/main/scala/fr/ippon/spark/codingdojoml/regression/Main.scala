cb1aa6415555293e2769e43d7e49bf564c855b74
Martin MENESTRET <mmenestret@ippon.fr>
2015-08-13 00:15:32 +0200

    Adding the One Hot Encoding, Standard Scaling, and implementation of a LinearReg and GBTReg

--- dojo-spark-ml/src/main/scala/fr/ippon/spark/codingdojoml/regression/Main.scala
+++ dojo-spark-ml/src/main/scala/fr/ippon/spark/codingdojoml/regression/Main.scala
@@ -1,6 +1,8 @@
 package fr.ippon.spark.codingdojoml.regression
 
 import fr.ippon.spark.codingdojoml.utils.FeatureEngineering
+import org.apache.spark.ml.regression.{GBTRegressor, LinearRegression}
+import org.apache.spark.ml.evaluation.RegressionEvaluator
 import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
@@ -21,13 +23,26 @@ object Main {
       .option("header", "true")
       .option("delimiter", ";")
       .option("inferSchema", "true")
-      .load("src/main/resources/bank-sample.csv")
-
-    println(FeatureEngineering.getMostFrequentCats(df))
-    println(FeatureEngineering.getMeans(df))
-    //FeatureEngineering.fillNas(df).show()
+      .load("src/main/resources/bank-full.csv")
 
     // feature engineering
+    val cleanedDF = FeatureEngineering.fillNas(df)
+    val featureDF = FeatureEngineering.createFeatureCol(cleanedDF)
 
+    val Array(training, prediction) = featureDF.randomSplit(Array(0.75, 0.25))
+
+    val lr = new LinearRegression().setLabelCol("age_label")
+    val lrModel = lr.fit(training)
+    val lrResult = lrModel.transform(prediction)
+
+    val gbtr = new GBTRegressor().setLabelCol("age_label")
+    val gbtrModel = gbtr.fit(training)
+    val gbtrResult = gbtrModel.transform(prediction)
+
+    val evaluator = new RegressionEvaluator()
+      .setLabelCol("age_label")
+      .setMetricName("rmse")
+    println(s"Gbtr precision: ${evaluator.evaluate(gbtrResult)}")
+    println(s"lr precision: ${evaluator.evaluate(lrResult)}")
   }
 }
--- dojo-spark-ml/src/main/scala/fr/ippon/spark/codingdojoml/utils/FeatureEngineering.scala
+++ dojo-spark-ml/src/main/scala/fr/ippon/spark/codingdojoml/utils/FeatureEngineering.scala
@@ -1,7 +1,11 @@
 package fr.ippon.spark.codingdojoml.utils
 
+import org.apache.spark.ml.Pipeline
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions
+import org.apache.spark.ml.feature.{StandardScaler, OneHotEncoder, VectorAssembler, StringIndexer}
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.sql.types.DoubleType
 
 /**
  * Created by Martin on 12/08/15.
@@ -28,12 +32,72 @@ object FeatureEngineering {
     meanDf.collect().head.getValuesMap[Double](numColsNames)
   }
 
-  def fillNas(df: DataFrame) = {
+  def fillNas(df: DataFrame): DataFrame = {
     var tempDF = df
     val mostFreqCats = getMostFrequentCats(df)
     val means = getMeans(df)
 
     catColsNames.foreach(c => tempDF = tempDF.na.replace(c, Map("unknown" -> mostFreqCats(c))))
     tempDF
+  }
+
+  def vectorizeNumCols(df: DataFrame): DataFrame = {
+    var tempDF = df
+    numColsNames.filter(c => c != "age")
+      .foreach(c => tempDF = new VectorAssembler()
+      .setInputCols(Array(c))
+      .setOutputCol(c+"_vectorized").transform(tempDF))
+    tempDF
+  }
+
+  def indexCatCols(df: DataFrame): DataFrame = {
+    val pipeline = new Pipeline()
+    val indexers = catColsNames.map{
+        c => new StringIndexer()
+          .setInputCol(c)
+          .setOutputCol(c+"_indexed")
+      }
+    pipeline.setStages(indexers.toArray)
+    pipeline.fit(df).transform(df)
+  }
+
+  def encodeCatCols(df: DataFrame): DataFrame = {
+    val pipeline = new Pipeline()
+    val encoders = catColsNames.map{
+      c => new OneHotEncoder()
+        .setInputCol(c+"_indexed")
+        .setOutputCol(c+"_encoded")
+    }
+    pipeline.setStages(encoders.toArray)
+    pipeline.fit(df).transform(df)
+  }
+
+  def scaleNumCols(df: DataFrame): DataFrame = {
+    val pipeline = new Pipeline()
+    val scalers = numColsNames
+      .filter(c => c != "age")
+      .map{
+        c => new StandardScaler()
+          .setInputCol(c+"_vectorized")
+          .setOutputCol(c+"_scaled")
+      }
+    pipeline.setStages(scalers.toArray)
+    val out = pipeline.fit(df).transform(df)
+    out.show()
+    out
+  }
+
+  def createFeatureCol(df: DataFrame): DataFrame = {
+    val indexedDF = indexCatCols(df)
+    val vectDF = vectorizeNumCols(indexedDF)
+    val encodedDF = encodeCatCols(vectDF)
+    val scaledDF = scaleNumCols(encodedDF)
+
+    val va = new VectorAssembler()
+
+
+    va.setInputCols((catColsNames.map(c => c+"_encoded") ++ numColsNames.filter(c => c != "age").map(c => c+"_scaled")).toArray)
+    va.setOutputCol("features")
+    va.transform(scaledDF).withColumn("age_label", df("age").cast(DoubleType))
   }
 }
