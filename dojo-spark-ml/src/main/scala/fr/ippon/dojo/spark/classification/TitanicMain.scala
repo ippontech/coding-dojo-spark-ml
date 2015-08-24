322692add18e59f8c42678fb6088f7c2d2ace0fe
Alexis Seigneurin <alexis@seigneurin.com>
2015-08-24 10:44:50 -0400

    - remove the code

--- dojo-spark-ml/src/main/scala/fr/ippon/dojo/spark/classification/TitanicMain.scala
+++ dojo-spark-ml/src/main/scala/fr/ippon/dojo/spark/classification/TitanicMain.scala
@@ -48,76 +48,36 @@ object TitanicMain {
     //////// Data cleansing
 
     // - calculate the mean age
+    //   use the calcMeanAge() function
     val meanAge = calcMeanAge(rawTraining, "Age")
 
-    // - calculate the most frequent "Embarked" value
-    val mostFrequentEmbarked = calcMostFrequentEmbarked(rawTraining, "Embarked")
-
     // - fill in missing "Age" values with the mean age (training+test dataset)
-    val trainingTemp1 = fillMissingAge(rawTraining, "Age", "Age_cleaned", meanAge)
-    val testTemp1 = fillMissingAge(rawTest, "Age", "Age_cleaned", meanAge)
+    //   -> use the fillMissingAge() function
 
-    // - fill in missing "Embarked" values with the most frequent value (training+test dataset)
-    val trainingTemp2 = fillMissingEmbarked(trainingTemp1, "Embarked", "Embarked_cleaned", mostFrequentEmbarked)
-    val testTemp2 = fillMissingEmbarked(testTemp1, "Embarked", "Embarked_cleaned", mostFrequentEmbarked)
-
-
-    val training = trainingTemp2
-    val test = testTemp2
 
     //////// Feature engineering
 
-    // - use a StringIndexer to convert the label column ("Survived") to a numeric column called "label"
-    val labelIndexerModel = new StringIndexer()
-      .setInputCol("Survived")
-      .setOutputCol("label")
-      .fit(training)
-    val trainingWithLabel = labelIndexerModel.transform(training)
+    // - use a StringIndexer to convert the label column ("Survived") to a numeric column called "label" (training data)
+    //   -> call the fit() method then the transform method()
 
     // - create a StringIndexer to transform the "Sex" column to a numeric column "Sex_indexed"
-    val sexIndexer = new StringIndexer().setInputCol("Sex").setOutputCol("Sex_indexed")
-
-    // - create a StringIndexer to transform the "Embarked_cleaned" column to a numeric column "Embarked_indexed"
-    val embarkedIndexer = new StringIndexer().setInputCol("Embarked_cleaned").setOutputCol("Embarked_indexed")
-
-    // - create a OneHotEncoder to transform the "Embarked_indexed" column to a vector column "Embarked_encoded"
-    val embarkedEncoder = new OneHotEncoder().setInputCol("Embarked_indexed").setOutputCol("Embarked_encoded")
 
 
     //////// Build the pipeline
 
     // - create a VectorAssembler to transform numeric columns into a vector
-    //   columns: "Pclass", "Sex_indexed", "Age", "Embarked_indexed"
-    val vectorAssembler = new VectorAssembler()
-      .setInputCols(
-        Array(
-          "Pclass", "Sex_indexed", "Age_cleaned"
-          //FIXME: "Pclass", "Sex_indexed", "Age_cleaned", "Fare", "Embarked_encoded"
-        ))
-      .setOutputCol("features")
+    //   columns: "Pclass", "Sex_indexed", "Age"
 
     // - create an Estimator and set its parameters (...)
     //   class: RandomForestClassifier
-    val randomForest = new RandomForestClassifier()
-      .setNumTrees(30)
 
-    // - create a Pipeline and set the stages (StringIndexers, VectorAssembler, RandomForestClassifier)
+    // - create a Pipeline and set the stages (StringIndexer, VectorAssembler, RandomForestClassifier)
     //   class: Pipeline
-    val pipeline = new Pipeline()
-      .setStages(
-        Array(
-          sexIndexer,
-          embarkedIndexer,
-          embarkedEncoder,
-          vectorAssembler,
-          randomForest
-        ))
 
 
     //////// Train the model
 
     // - fit the model on the training dataset: fit()
-    val model = pipeline.fit(trainingWithLabel)
 
     // - optional: display the model
     //println(model.asInstanceOf[PipelineModel].stages(4).asInstanceOf[RandomForestClassificationModel].toDebugString)
@@ -125,15 +85,14 @@ object TitanicMain {
     //////// Apply the model
 
     // - apply the model on the test dataset: transform()
-    val predictions = model.transform(test)
 
     // - compare the predictions with the expected results
-    predictions
-      .join(expected, "PassengerId")
-      .select("Survived", "prediction")
-      .groupBy("Survived", "prediction")
-      .agg(count("*"))
-      .show()
+    //    predictions
+    //      .join(expected, "PassengerId")
+    //      .select("Survived", "prediction")
+    //      .groupBy("Survived", "prediction")
+    //      .agg(count("*"))
+    //      .show()
 
 
   }
